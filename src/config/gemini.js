7b5683d6b697e1488f13feaa3831ef5e01c613f9
KajalDeore04 <kajaldeore04@gmail.com>
2025-01-13 14:40:07 +0530

    host

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -3,8 +3,11 @@ import {
     HarmCategory,
     HarmBlockThreshold,
   } from "@google/generative-ai";
+  import dotenv from "dotenv";
+
+  dotenv.config();
   
-  const apiKey = "Your API Key";
+  const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
